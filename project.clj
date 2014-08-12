b0d5403b7896c82d629bd2c5c3dadb31a2e8a4f0
Naohisa Takahashi <hakuro16@gmail.com>
2014-08-12 23:04:47 +0900

    initial commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject realtime-chat-api "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]
+                 [aleph "0.3.3"]
+                 [clojure-msgpack "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler realtime-chat-api.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
