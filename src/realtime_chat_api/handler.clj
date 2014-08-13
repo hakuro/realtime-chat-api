11e85ad1ca339c7a29300e806e17dce6953ab9af
Naohisa Takahashi <hakuro16@gmail.com>
2014-08-13 23:33:12 +0900

    add aleph init to handler

--- src/realtime_chat_api/handler.clj
+++ src/realtime_chat_api/handler.clj
@@ -1,7 +1,34 @@
 (ns realtime-chat-api.handler
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.tools.logging :as logging]
+            [lamina.core :refer :all]
+            [aleph.http :refer :all]
+            [environ.core :refer [env]]))
+
+(def broadcast-channel (channel))
+
+(defn handler [ch handshake]
+  (receive-all ch (fn [msg] (enqueue broadcast-channel msg)))
+  (receive-all broadcast-channel (fn [msg] (enqueue ch msg))))
+
+(defonce server (atom nil))
+
+(defn start [port]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (reset! server
+            (start-http-server handler
+                               {:port port
+                                :websocket true}))
+    (logging/info (str "Started with port " port))))
+
+(defn stop []
+  (when @server
+    (@server)
+    (reset! server nil)))
+
+(defn init [port] (start port))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
